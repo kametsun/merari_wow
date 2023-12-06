234a1fe3535b91fffb41fa3ad8f692ab673d49fd
kametsun <kametsuba.0713@gmail.com>
2023-12-06 18:06:58 +0900

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/index'
+  get 'buys/index'
+  get 'nfts/index'
+  get 'nfts/show'
+  root to: 'homes#index'
+
+  get 'login' => 'users#login'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
