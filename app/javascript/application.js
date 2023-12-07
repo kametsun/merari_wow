bfb1926142d8c92e8e7a2188c85a9c627422d3e2
kametsun <kametsuba.0713@gmail.com>
2023-12-07 21:58:38 +0900

    js vote ok

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,27 +3,7 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 // すべての投票ボタンを取得
-const voteButtons = document.querySelectorAll('.vote-button');
 
-// 各ボタンにクリックイベントリスナーを追加
-voteButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const idolId = button.dataset.id;
-    const modal = document.querySelector(`#modal-${idolId}`);
-    modal.classList.add('is-open');
-  });
-});
-
-// すべてのモーダルクローズボタンを取得
-const modalCloses = document.querySelectorAll('.js-close-button');
-
-// 各クローズボタンにクリックイベントリスナーを追加
-modalCloses.forEach((button) => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.js-modal');
-    modal.classList.remove('is-open');
-  });
-});
 
 document.addEventListener("turbo:load", function () {
   var counter = document.getElementById('token');
@@ -37,4 +17,26 @@ document.addEventListener("turbo:load", function () {
   }
 
   setInterval(increase, 100);
+
+  const voteButtons = document.querySelectorAll('.vote-button');
+
+  // 各ボタンにクリックイベントリスナーを追加
+  voteButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const idolId = button.dataset.id;
+      const modal = document.querySelector(`#modal-${idolId}`);
+      modal.classList.add('is-open');
+    });
+  });
+
+  // すべてのモーダルクローズボタンを取得
+  const modalCloses = document.querySelectorAll('.js-close-button');
+
+  // 各クローズボタンにクリックイベントリスナーを追加
+  modalCloses.forEach((button) => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.js-modal');
+      modal.classList.remove('is-open');
+    });
+  });
 });
