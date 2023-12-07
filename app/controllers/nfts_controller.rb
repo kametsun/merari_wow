f880552f87d19fce0dfd0eb6ec74188a3a1aa92f
kametsun <kametsuba.0713@gmail.com>
2023-12-08 01:52:38 +0900

    player

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -8,6 +8,7 @@ class NftsController < ApplicationController
   end
 
   def firstGacha
+    User.find(1).token += 10
     redirect_to nft_path(Nft.find(1).id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_075322) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.integer "token"
+    t.integer "token", default: 0
     t.boolean "is_login", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
