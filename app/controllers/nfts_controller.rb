e78bc8c95709fe811ee810eb83e9085364872d70
kametsun <kametsuba.0713@gmail.com>
2023-12-08 12:20:24 +0900

    account

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,21 +1,51 @@
 class NftsController < ApplicationController
   skip_before_action :getUser, only: :firstGacha
   def index
-    @nfts = Nft.all
+    @nfts = Nft.where.not(id: [2, 3])
   end
 
   def show
     @nft = Nft.find(params[:id])
+
+    # idが1と2のときだけtoken付与
+    if @nft.id == 2
+      cookies.delete(:user_id)
+      sleep(2)
+      # QRコード用にアカウント追加
+      createNewUser
+      @user.token = @user.token + @nft.token
+      #@user.update
+    end
+
+    if @nft.id == 3
+      cookies.delete(:user_id)
+      sleep(2)
+      # QRコード用にアカウント追加
+      createNewUser
+
+      @user.token = @user.token + @nft.token
+      #@user.update
+    end
   end
 
-  def firstGacha
+  def createNewUser
     user = User.new(name: "エンドユーザ#{User.all.size + 1}", token: 0)
     user.save!
     # saveとレンダリングに時間がかかるから時間をおく
     sleep(2)
     cookies[:user_id] = user.id
+  end
 
-    user.update(token: user.token+10)
-    redirect_to nft_path(Nft.find(1).id)
+  def firstGacha
+    user = User.new(name: "エンドユーザ#{User.all.size + 1}", token: 0)
+    user.save!
+    # 初回NFT付与
+    nft = Nft.find(1)
+    nft.save!(user_id: user.id)
+    # saveとレンダリングに時間がかかるから時間をおく
+    sleep(2)
+    cookies[:user_id] = user.id
+    user.update(token: user.token + nft.token)
+    redirect_to nft_path(nft.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_173837) do
 
   create_table "nfts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
+    t.integer "token"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
